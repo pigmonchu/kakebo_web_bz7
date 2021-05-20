39f53ba7c35618e26cce86b99386108855bef304
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2021-05-21 00:39:50 +0200

    Primer select

--- kakebo/views.py
+++ kakebo/views.py
@@ -1,5 +1,26 @@
 from kakebo import app
+import sqlite3
 
 @app.route('/')
 def index():
-    return 'Flask rulando'
+    conexion = sqlite3.connect("movimientos.db")
+    cur = conexion.cursor()
+
+    cur.execute("SELECT * FROM movimientos;")
+
+    claves = cur.description
+    
+    filas = cur.fetchall()
+    d = {}
+
+
+
+
+
+    l = []
+    for fila in filas:
+        d = {}
+        for columna in fila:
+    conexion.close()
+
+    return "Consulta realizada"
